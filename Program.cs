667e65264befca8d7b141743243c2da6a74862b0
Tuhin <towhid.tuhin@selise.ch>
2020-02-02 22:45:45 +0600

    GCD wrost case implementation.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,53 @@ namespace GCD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 8703029615, 870302961955, 870302961962, 870302961969, 870302961976 };
+
+
+
+            var lowestnumber = numbers[0];
+            long gcd = 0;
+
+            //for (long i = 1; i < numbers.Length; i++)
+            //{
+            //    gcd = GetGcd(numbers[i], gcd);
+            //}
+            //Console.WriteLine($"GCD Is: {gcd}");
+            //Console.ReadLine();
+
+            for (long i=1; i<numbers.Length; i++)
+            {
+                if(lowestnumber >= numbers[i])
+                {
+                    lowestnumber = numbers[i];
+                }
+            }
+            for(long i = lowestnumber; i >= 1; i--)
+            {
+                for (long j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[j] % i  != 0)
+                    {
+                        gcd = 0;
+                        break;
+                    }
+                    gcd = i;
+                }
+                if(gcd != 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"GCD Is: {gcd}");
+            Console.ReadLine();
         }
+        //static long GetGcd(long x, long y)
+        //{
+        //    if (x == 0)
+        //    {
+        //        return y;
+        //    }
+        //    return GetGcd(y % x, x);
+        //}
     }
 }
